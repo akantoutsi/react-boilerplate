903db3cee5be721f6c027f604a0c3ae54f6490fd
akantoutsi <a.kantoutsi@gmail.com>
2020-01-19 18:37:36 +0200

    Updates

---
+++ precache-manifest.313e6c3bc2082e18130a4523a6094b03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0446354632c9fd0797a3a85042dae20b",
+    "url": "/react-boilerplate/index.html"
+  },
+  {
+    "revision": "530ac30c40e775173c15",
+    "url": "/react-boilerplate/static/css/main.cab4e29b.chunk.css"
+  },
+  {
+    "revision": "4dd6dbb0b85e8e1bba5e",
+    "url": "/react-boilerplate/static/js/2.9f66315e.chunk.js"
+  },
+  {
+    "revision": "530ac30c40e775173c15",
+    "url": "/react-boilerplate/static/js/main.b7d95aea.chunk.js"
+  },
+  {
+    "revision": "1186aae16f7357f2bb5a",
+    "url": "/react-boilerplate/static/js/runtime-main.3d7ada59.js"
+  },
+  {
+    "revision": "ec646854ab903b900ca2d2a8c5030b27",
+    "url": "/react-boilerplate/static/media/tv.ec646854.svg"
+  }
+]);
